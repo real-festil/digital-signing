782ef13a004fd4fb6e324c2d370ae5e93aac1680
Роман Гладышев <romagladyshev@gmail.com>
2021-06-09 14:39:01 +0600

    add p12 support

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 const crypto = require('crypto');
-const fs = require('fs');
+const p12 = require('p12-pem');
 
-exports.signFile = function (private_key, doc) {
+exports.signFile = function (cert_path, password, doc) {
+  const {pemKey} = p12.getPemFromP12(cert_path, password);
   const signer = crypto.createSign('RSA-SHA256');
   signer.write(doc);
   signer.end();
 
-  const signature = signer.sign(private_key, 'base64');
+  const signature = signer.sign('-----BEGIN RSA PRIVATE KEY-----\n' +pemKey.split('-----')[2] + '\n-----END RSA PRIVATE KEY-----', 'base64');
 
   return signature;
 }
 
-exports.verifyFile = function (public_key, signature, doc) {
+exports.verifyFile = function (cert_path, password, signature, doc) {
+  const {pemCertificate} = p12.getPemFromP12(cert_path, password);
+  const public_key = crypto.createPublicKey('-----BEGIN CERTIFICATE-----\n' +pemCertificate.split('-----')[2] + '\n-----END CERTIFICATE-----').export({type:'spki', format:'pem'})
   const verifier = crypto.createVerify('RSA-SHA256');
   verifier.write(doc);
   verifier.end();
